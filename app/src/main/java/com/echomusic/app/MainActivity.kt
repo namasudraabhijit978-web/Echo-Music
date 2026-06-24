2dbee33ce0c7d8fedee97f3965c73e0e2f9c8aad
namasudraabhijit978-web <namasudraabhijit978@gmail.com>
2026-06-24 21:08:45 +0530

    Update MainActivity.kt

--- app/src/main/java/com/echomusic/app/MainActivity.kt
+++ app/src/main/java/com/echomusic/app/MainActivity.kt
@@ -23,6 +23,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.echomusic.app.ui.screens.HomeScreen
+import com.echomusic.app.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -35,7 +38,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    
+                    val mainViewModel: MainViewModel = hiltViewModel()
+                    HomeScreen(viewModel = mainViewModel)
                 }
             }
         }
