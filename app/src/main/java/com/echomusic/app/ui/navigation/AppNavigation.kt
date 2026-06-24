ad45f273345480f90ea81b30d9188a126dc4232b
namasudraabhijit978-web <namasudraabhijit978@gmail.com>
2026-06-24 22:36:31 +0530

    Create app/src/main/java/com/echomusic/app/ui/navigation/AppNavigation.kt

---
+++ app/src/main/java/com/echomusic/app/ui/navigation/AppNavigation.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2026 Chronos Developers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.echomusic.app.ui.navigation
+
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.echomusic.app.ui.screens.HomeScreen
+import com.echomusic.app.ui.screens.PlayerScreen
+import com.echomusic.app.viewmodel.MainViewModel
+
+@Composable
+fun AppNavigation(viewModel: MainViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(
+                viewModel = viewModel,
+                onNavigateToPlayer = {
+                    navController.navigate("player")
+                }
+            )
+        }
+        
+        composable(
+            route = "player",
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Up,
+                    animationSpec = tween(300)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Down,
+                    animationSpec = tween(300)
+                )
+            }
+        ) {
+            PlayerScreen(
+                viewModel = viewModel,
+                onClose = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
