9ba6b8b1e05ff086ce545eb72457bb15aff77001
namasudraabhijit978-web <namasudraabhijit978@gmail.com>
2026-06-24 22:46:23 +0530

    Create app/src/main/java/com/echomusic/app/utils/ColorExtractor.kt

---
+++ app/src/main/java/com/echomusic/app/utils/ColorExtractor.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2026 Chronos Developers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.echomusic.app.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.compose.ui.graphics.Color
+import androidx.palette.graphics.Palette
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+object ColorExtractor {
+    suspend fun getDominantColor(context: Context, uri: Uri): Color? = withContext(Dispatchers.IO) {
+        try {
+            val bitmap: Bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                val source = ImageDecoder.createSource(context.contentResolver, uri)
+                ImageDecoder.decodeBitmap(source) { decoder, _, _ ->
+                    decoder.allocator = ImageDecoder.ALLOCATOR_SOFTWARE
+                    decoder.isMutableRequired = true
+                }
+            } else {
+                @Suppress("DEPRECATION")
+                MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+            }
+
+            val palette = Palette.from(bitmap).generate()
+            val rgb = palette.dominantSwatch?.rgb ?: palette.mutedSwatch?.rgb
+            rgb?.let { Color(it) }
+        } catch (e: Exception) {
+            null
+        }
+    }
+}
